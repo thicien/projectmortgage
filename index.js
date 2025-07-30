69bb4798651428fb2f5b65a0d23fc175e1210140
mugisha thicien <mugishathicien04@gmail.com>
2025-07-30 14:12:00 +0200

    staged

---
+++ index.js
@@ -0,0 +1,4 @@
+const element = document.getElementById("example");
+console.log("innerHtml", element.innerHtml);
+console.log("innerText", element.innerText);
+console.log("textContent", element.textContent);
