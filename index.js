214c6bcea9ad097d77d6fb851bfad652207aaa86
mugisha thicien <mugishathicien04@gmail.com>
2025-07-30 14:15:02 +0200

    added

--- index.js
+++
@@ -1,4 +0,0 @@
-const element = document.getElementById("example");
-console.log("innerHtml", element.innerHtml);
-console.log("innerText", element.innerText);
-console.log("textContent", element.textContent);
